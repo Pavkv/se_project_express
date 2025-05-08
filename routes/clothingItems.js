473108559a481a9a20b67185bd0419767e7b7f06
Pavel Zobov <pavelzobov123@gmail.com>
2025-05-08 09:27:34 -0400

    fix: Update validation

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -8,11 +8,7 @@ const auth = require('../middlewares/auth');
 const {validateClothingItemBody, validateId} = require("../middlewares/validation");
 
 router.get('/', getClothingItems);
-router.post('/', auth, (req, res, next) => {
-  console.log(req.body);
-  next();
-},
-  validateClothingItemBody(), createClothingItem);
+router.post('/', auth, validateClothingItemBody(), createClothingItem);
 router.delete('/:itemId', validateId(), auth,  deleteClothingItem);
 router.put('/:itemId/likes', validateId(), auth, likeItem);
 router.delete('/:itemId/likes', validateId(), auth, dislikeItem);
